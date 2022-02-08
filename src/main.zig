c2afa891246fc8d413d0c859fba265434fe86808
Jeffery Stager <jffry09@gmail.com>
2022-02-08 00:12:10 -0500

    LUT generation working

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,81 @@
 const std = @import("std");
+const Color = @import("image_types.zig").Color;
 const ppm = @import("ppm.zig");
+const PpmData = ppm.PpmData;
+
+fn find_best_match(target: Color, palette: []Color) usize {
+    var match: usize = 0;
+    var min_dist: usize = std.math.maxInt(usize);
+
+    const pow = std.math.pow;
+    for (palette) |color, i| {
+        // The square root in the Euclidean distance can be removed here because
+        // we just care about relative distances
+        const r1 = @intCast(i64, color.r);
+        const r2 = @intCast(i64, target.r);
+        const g1 = @intCast(i64, color.g);
+        const g2 = @intCast(i64, target.g);
+        const b1 = @intCast(i64, color.b);
+        const b2 = @intCast(i64, target.b);
+        var dist = pow(i64, r1 - r2, 2);
+        dist += pow(i64, g1 - g2, 2);
+        dist += pow(i64, b1 - b2, 2);
+
+        if (dist < min_dist) {
+            match = i;
+            min_dist = @intCast(usize, dist);
+        }
+    }
+
+    return match;
+}
+
+fn generate_lut(
+    allocator: std.mem.Allocator,
+    palette: PpmData,
+    n_shades: u32,
+) !PpmData {
+    var colormap = std.AutoHashMap(Color, bool).init(allocator);
+    var lut = try allocator.alloc(
+        Color,
+        palette.width * palette.height * n_shades,
+    );
+    var index: usize = 0;
+
+    for (palette.data) |color| {
+        if (colormap.get(color)) |_| {
+            continue;
+        }
+
+        try colormap.put(color, true);
+
+        var i = n_shades;
+        while (i > 0) : (i -= 1) {
+            const r: u32 = (color.r * i + (n_shades / 4)) / (n_shades / 2);
+            const g: u32 = (color.g * i + (n_shades / 4)) / (n_shades / 2);
+            const b: u32 = (color.b * i + (n_shades / 4)) / (n_shades / 2);
+
+            var target = Color{
+                .r = @intCast(u8, std.math.clamp(r, 0, 255)),
+                .g = @intCast(u8, std.math.clamp(g, 0, 255)),
+                .b = @intCast(u8, std.math.clamp(b, 0, 255)),
+            };
+            lut[index] = palette.data[find_best_match(target, palette.data)];
+            index += 1;
+        }
+    }
+
+    var result: PpmData = undefined;
+    try result.init(
+        allocator,
+        n_shades,
+        palette.width * palette.height,
+        palette.depth,
+        lut,
+    );
+
+    return result;
+}
 
 pub fn main() anyerror!void {
     const allocator = std.heap.c_allocator;
@@ -13,11 +89,12 @@ pub fn main() anyerror!void {
     defer palette_file.close();
 
     const palette = try ppm.parse_file(allocator, palette_file);
+    defer palette.deinit();
 
-    for (palette) |color| {
-        std.log.info(
-            "{{ {d:3} {d:3} {d:3} }}",
-            .{ color.r, color.g, color.b },
-        );
-    }
+    const lut = try generate_lut(allocator, palette, 64);
+    defer lut.deinit();
+
+    const out_file = try std.fs.cwd().createFile("lut.ppm", .{});
+    defer out_file.close();
+    try ppm.write_file(allocator, out_file, &lut);
 }
--- src/ppm.zig
+++ src/ppm.zig
@@ -1,6 +1,35 @@
 const std = @import("std");
 const Color = @import("image_types.zig").Color;
 
+pub const PpmData = struct {
+    allocator: std.mem.Allocator,
+    width: u32,
+    height: u32,
+    depth: u32,
+    data: []Color,
+
+    pub fn init(
+        self: *PpmData,
+        allocator: std.mem.Allocator,
+        width: u32,
+        height: u32,
+        depth: u32,
+        data: []Color,
+    ) !void {
+        self.* = PpmData{
+            .allocator = allocator,
+            .width = width,
+            .height = height,
+            .depth = depth,
+            .data = data,
+        };
+    }
+
+    pub fn deinit(self: *const PpmData) void {
+        self.allocator.free(self.data);
+    }
+};
+
 fn validate_header(reader: anytype) !u8 {
     var buf: [2]u8 = undefined;
     const len = try reader.read(&buf);
@@ -55,17 +84,17 @@ fn get_file_info(
     return ch;
 }
 
-fn convert_data_to_colors(palette_data: []u8, palette: []Color) !void {
-    if (palette_data.len != (palette.len * 3)) return error.BadConversion;
-    if ((palette_data.len % 3) != 0) return error.BadPaletteData;
+fn convert_data_to_colors(color_data: []u8, colors: []Color) !void {
+    if (color_data.len != (colors.len * 3)) return error.BadConversion;
+    if ((color_data.len % 3) != 0) return error.BadColorData;
 
-    var data = palette_data[0..palette_data.len];
+    var data = color_data[0..color_data.len];
     var counter: usize = 0;
     while (data.len > 0) : ({
         data = data[3..data.len];
         counter += 1;
     }) {
-        palette[counter] = Color{
+        colors[counter] = Color{
             .r = data[0],
             .g = data[1],
             .b = data[2],
@@ -73,7 +102,18 @@ fn convert_data_to_colors(palette_data: []u8, palette: []Color) !void {
     }
 }
 
-pub fn parse_file(allocator: std.mem.Allocator, file: std.fs.File) ![]Color {
+fn convert_colors_to_data(colors: []Color, color_data: []u8) !void {
+    if (color_data.len != (colors.len * 3)) return error.BadConversion;
+    if ((color_data.len % 3) != 0) return error.BadColorData;
+
+    for (colors) |color, i| {
+        color_data[(i * 3) + 0] = color.r;
+        color_data[(i * 3) + 1] = color.g;
+        color_data[(i * 3) + 2] = color.b;
+    }
+}
+
+pub fn parse_file(allocator: std.mem.Allocator, file: std.fs.File) !PpmData {
     const reader = file.reader();
 
     var width: u32 = undefined;
@@ -81,25 +121,43 @@ pub fn parse_file(allocator: std.mem.Allocator, file: std.fs.File) ![]Color {
     var depth: u32 = undefined;
 
     var ch = try get_file_info(reader, &width, &height, &depth);
-    std.log.info("width: {}", .{width});
-    std.log.info("height: {}", .{height});
-    std.log.info("depth: {}", .{depth});
 
-    var palette_data: []u8 = try allocator.alloc(u8, width * height * 3);
-    defer allocator.free(palette_data);
+    var color_data: []u8 = try allocator.alloc(u8, width * height * 3);
+    defer allocator.free(color_data);
 
-    var palette: []Color = try allocator.alloc(Color, width * height);
+    var colors: []Color = try allocator.alloc(Color, width * height);
 
     ch = try skip_whitespace_and_comments(ch, reader);
-    palette_data[0] = ch;
-    var read = try reader.read(palette_data[1..palette_data.len]);
-    if ((read + 1) != palette_data.len) {
-        for (palette_data[read + 1 .. palette_data.len]) |*byte| {
+    color_data[0] = ch;
+    var read = try reader.read(color_data[1..color_data.len]);
+    if ((read + 1) != color_data.len) {
+        for (color_data[read + 1 .. color_data.len]) |*byte| {
             byte.* = 0;
         }
     }
 
-    try convert_data_to_colors(palette_data, palette);
+    try convert_data_to_colors(color_data, colors);
 
-    return palette;
+    var result: PpmData = undefined;
+    try result.init(allocator, width, height, depth, colors);
+
+    return result;
+}
+
+pub fn write_file(
+    allocator: std.mem.Allocator,
+    file: std.fs.File,
+    data: *const PpmData,
+) !void {
+    const writer = file.writer();
+
+    var color_data = try allocator.alloc(u8, data.width * data.height * 3);
+    try convert_colors_to_data(data.data, color_data);
+
+    try std.fmt.format(
+        writer,
+        "P6 {} {} {} ",
+        .{ data.width, data.height, data.depth },
+    );
+    _ = try writer.write(color_data);
 }
